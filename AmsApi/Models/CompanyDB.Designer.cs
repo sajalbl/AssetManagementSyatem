f634eef57ca88eed873d74c0dc657d36b55978b0
SaJaL <sajal@bluelupin.com>
2015-10-23 12:33:37 +0530

    fix

--- AmsApi/Models/CompanyDB.Designer.cs
+++ AmsApi/Models/CompanyDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Code\AssetManagementSyatem\AmsApi\Models\CompanyDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\admin\documents\visual studio 2013\Projects\AssetManagementSystem\AmsApi\Models\CompanyDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
