957e03da094df01492fd2df1ca611bf171ecbe6f
SaJaL <sajal@bluelupin.com>
2015-11-26 19:18:59 +0530

    upload file service and context

---
+++ AssetManagementSystem/app/services/uploadFileService.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+app.factory('uploadFileService', ['$http', function ($http) {
+
+    var serviceBase = 'http://localhost:14597/';
+    var uploadFileServiceFactory = {};
+
+    var _fileUpload = function (empid, file, path) {
+        var data = new FormData();
+        data.append('EmployeeID', empid);
+        data.append('UploadImage', file);
+        data.append('FolderPath', path);
+        return $http.post(serviceBase + 'api/manage/imageUpload', data, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }).then(function (results) {
+        });
+    };
+    uploadFileServiceFactory.fileUpload = _fileUpload;
+
+    return uploadFileServiceFactory;
+}]);
