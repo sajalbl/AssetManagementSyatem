6cb4010de98420e026811ab548b056b326d17ed8
SaJaL <sajal@bluelupin.com>
2015-12-15 11:47:20 +0530

    fix

---
+++ AmsApi/Adapter/DeleteCompanyAdapter.cs
@@ -0,0 +1,41 @@
+﻿using AmsApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http; 
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;               
+using System.Data; 
+namespace AmsApi.Adapter
+{  
+    public class DeleteCompanyAdapter
+    {
+
+          
+        public ManageCompanyResponse deleteCompany(ManageCompanyRequest request)
+        {
+            ManageCompanyResponse response = new ManageCompanyResponse();
+
+           Company_table company = new Company_table();
+            using (var context = new Company_dbEntities())
+            {
+
+                company = (from a in context.Company_table where request.CompanyName == a.CompanyName && request.OwnerName == a.OwnerName select a).FirstOrDefault<Company_table>();
+               
+                context.Company_table.Remove(company);
+                
+                context.SaveChanges();
+                
+            }
+        
+            return response;
+        }
+
+    }
+}
---
+++ AssetManagementSystem/app/controllers/companyEditController.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+app.controller('companyEditController', ['$scope', '$http', 'localStorageService', function ($scope, $http, localStorageService) {
+    var serviceBase = 'http://localhost:14597/';
+  
+    $scope.companyInfo = localStorageService.get("CompanyInfo");
+    $scope.message = '';
+    
+    $scope.update = function () {
+        var text = { "CompanyName": $scope.companyInfo.CompanyName, "OwnerName": $scope.companyInfo.OwnerName, "Address": $scope.companyInfo.Address, "Contact": $scope.companyInfo.Contact, "Email": $scope.companyInfo.Email };
+
+        $http.post(serviceBase + 'api/manage/updateCompany', JSON.stringify(text)).then(function (results) {
+            if (results.data != null && results.data.IsCompanyUpdated) {
+                //Re-store the cookie after successful update.
+                localStorageService.set("CompanyInfo", $scope.companyInfo)
+                $scope.message = "Details Updated Successfully";
+            }
+        });
+    };
+
+   //Add a validation method here for update.
+
+
+}]);
